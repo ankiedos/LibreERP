34fc928911093a6617498637a5ab0b7a1d454d73
ankiedos <antek_kiedos@outlook.com>
2023-10-22 11:46:12 +0200

    Started core module: user, macros, e-mail, postal-code, address

---
+++ core/utils/address.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "postal-code.hpp"
+
+namespace CppERP
+{
+    class Address
+    {
+        std::string country_;
+        std::string region_;
+        std::string city_;
+        std::string street_;
+        std::string building_;
+        std::string apartment_;
+        PostalCode postal_code_;
+    public:
+        explicit Address() = default;
+        Address(const Address& other)
+        {
+            country_ = other.country_;
+            region_ = other.region_;
+            city_ = other.city_;
+            street_ = other.street_;
+            building_ = other.building_;
+            apartment_ = other.apartment_;
+            postal_code_ = other.postal_code_;
+        }
+        explicit Address(Address&&) = default;
+        Address& operator=(const Address&) = default;
+    };
+}
